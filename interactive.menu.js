99ee488d2932b60c5d7a9d9f451471da183408bd
Andrey Arkhipov <wibox@ya.ru>
2013-10-25 12:50:16 +0400

    work version

--- interactive.menu.js
+++ interactive.menu.js
@@ -3,23 +3,20 @@
         function с() {
             var windowTop = $(window).scrollTop();
             var menuItems = $("#interactive-menu li a");
-            var a = [], page;
+            var paddingTop = 100;
+            var a = [];
+            var page;
             for (var i= 0, size = menuItems.length; i<size; i++) {
                 var b = menuItems.eq(i).attr("href");
                 a.push(b);
-                page = $(b).offset().top;
-                console.log(windowTop);
-                console.log(page);
+                page = $(b).offset().top - paddingTop;
                 if (windowTop > page) {
-                    $(this).addClass("active");
+                    menuItems.removeClass("active");
+                    menuItems.eq(i).addClass("active");
                 }
                 console.log(b);
             }
             console.log(size);
-            $("#interactive-menu li a").removeClass("active");
-
-
-
         }
         $(function(){
             $(window).scroll(с);
