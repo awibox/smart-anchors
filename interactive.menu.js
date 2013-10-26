f58b8ab3a8d0a352b799051a76b645241721641e
Andrey Arkhipov <wibox@ya.ru>
2013-10-27 01:17:31 +0400

    Почистил код, переименвал функции, переменные.

--- interactive.menu.js
+++ interactive.menu.js
@@ -1,24 +1,23 @@
 (function( $ ) {
-    jQuery.fn.interactiveMenu = function(padding) {
-        var d = this.selector;
-        var paddingTop = padding;
-        function с() {
+    jQuery.fn.FixedIN = function(offset) {
+        var idFixedIN = this.selector;
+        var offsetTop = offset;
+        function scrollPage() {
             var windowTop = $(window).scrollTop();
-            var menuItems = $(d).find("a");
+            var navItems = $(idFixedIN).find("a");
             var page;
-            for (var i= 0, size = menuItems.length; i<size; i++) {
-                var b = menuItems.eq(i).attr("href");
-                page = $(b).offset().top - paddingTop;
+            for (var i= 0, size = navItems.length; i<size; i++) {
+                var itemContent = navItems.eq(i).attr("href");
+                page = $(itemContent).offset().top - offsetTop;
                 if (windowTop > page) {
-                    menuItems.removeClass("active");
-                    menuItems.eq(i).addClass("active");
+                    navItems.removeClass("active");
+                    navItems.eq(i).addClass("active");
                 }
-                console.log(b);
             }
-            console.log(size);
         }
         $(function(){
-            $(window).scroll(с);
+            $(window).scroll(scrollPage);
         });
+
     };
 })(jQuery);
