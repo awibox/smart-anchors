6948bf8a020615268e4c8e7a0248aeb959adc320
Andrey Arkhipov <wibox@ya.ru>
2013-10-29 12:57:04 +0400

    Сделал систему автоматического просчета offsetTop.

--- fixedin.js
+++ fixedin.js
@@ -1,21 +1,29 @@
 (function( $ ) {
-    jQuery.fn.fixedin = function(offset) {
+    jQuery.fn.fixedin = function() {
         var idFixedin = this.selector;
-        var offsetTop = offset;
+        var offsetTop = 100;
         function scrollPage() {
             var windowTop = $(window).scrollTop();
-            var navItems = $(idFixedin).find("a");
             var page;
             var itemContentHeight;
             var windowHeight;
+            var itemOffset;
+            var navItems = $(idFixedin).find("a");
             for (var i= 0, size = navItems.length; i<size; i++) {
                 var itemContent = navItems.eq(i).attr("href");
                 page = $(itemContent).offset().top - offsetTop;
-                if (i == navItems.length-1) {
-                    itemContentHeight = $(itemContent).height();
-                    windowHeight = $(window).height();
-                    page = $(itemContent).offset().top - offsetTop - windowHeight + itemContentHeight;
+                itemContentHeight = $(itemContent).height();
+                windowHeight = $(window).height();
+                itemOffset = windowHeight - itemContentHeight;
+                if(itemOffset>offsetTop) {
+                    page = $(itemContent).offset().top - itemOffset;
+                    if(navItems.length-1 == i) {
+                        page = $(itemContent).offset().top - offsetTop - itemOffset;
+                    }
+                } else {
+                    page = $(itemContent).offset().top - offsetTop;
                 }
+                $(itemContent).css('min-height', windowHeight/1.5)
                 if (windowTop > page) {
                     navItems.removeClass("active");
                     navItems.eq(i).addClass("active");
@@ -25,7 +33,7 @@
         $(function(){
             scrollPage();
             $(window).scroll(scrollPage);
+            $(window).resize(scrollPage);
         });
-
     };
 })(jQuery);
