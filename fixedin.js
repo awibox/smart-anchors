2405edb4d0e01d62edcd79769216d89603ccc2f1
Andrey Arkhipov <wibox@ya.ru>
2013-10-29 12:15:26 +0400

    Исправлен баг с последним пунктом

--- fixedin.js
+++ fixedin.js
@@ -6,9 +6,16 @@
             var windowTop = $(window).scrollTop();
             var navItems = $(idFixedin).find("a");
             var page;
+            var itemContentHeight;
+            var windowHeight;
             for (var i= 0, size = navItems.length; i<size; i++) {
                 var itemContent = navItems.eq(i).attr("href");
                 page = $(itemContent).offset().top - offsetTop;
+                if (i == navItems.length-1) {
+                    itemContentHeight = $(itemContent).height();
+                    windowHeight = $(window).height();
+                    page = $(itemContent).offset().top - offsetTop - windowHeight + itemContentHeight;
+                }
                 if (windowTop > page) {
                     navItems.removeClass("active");
                     navItems.eq(i).addClass("active");
