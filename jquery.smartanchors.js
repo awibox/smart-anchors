3b3891ba0245b9e1eb8b7c2764d111eb2549ab6c
Andrey Arkhipov <wibox@ya.ru>
2013-10-31 00:50:47 +0400

    Сократил JS-код.

--- jquery.smartanchors.js
+++ jquery.smartanchors.js
@@ -7,11 +7,11 @@
  *
  * Licensed under the MIT license.
  *
- * $('nav_id').smartanchors(speed);
+ * $('nav_id').smartanchors(offset, speed);
  */
 (function($) {
     jQuery.fn.smartanchors = function(speed) {
-        var idFixedin = this.selector;
+        var idAnchors = this.selector;
         var offsetTop = 100;
         var page,
             itemContent,
@@ -19,38 +19,28 @@
             windowHeight,
             itemOffset,
             navItems,
-            windowTop,
-            i;
+            windowTop;
         function scrollPage() {
             windowTop = $(window).scrollTop();
-            navItems = $(idFixedin).find("a");
-            for (i= 0, size = navItems.length; i<size; i++) {
+            navItems = $(idAnchors).find("a");
+            for (var i= 0, size = navItems.length; i<size; i++) {
                 itemContent = navItems.eq(i).attr("href");
                 page = $(itemContent).offset().top - offsetTop;
-                itemContentHeight = $(itemContent).height();
-                windowHeight = $(window).height();
-                itemOffset = windowHeight - itemContentHeight;
-
-                if(itemOffset<offsetTop) {
-                    page = $(itemContent).offset().top - itemOffset;
-                } else {
-                    page = $(itemContent).offset().top - offsetTop;
-                }
-                if(navItems.length-1 == i) {
-                    page = $(itemContent).offset().top - offsetTop - itemOffset;
-                    $(itemContent).css('min-height', windowHeight/1.5)
-                }
                 if(navItems.length-2 == i) {
                     $(itemContent).css('min-height', windowHeight/1.5)
                     console.log(windowHeight)
                 }
-
+                if(navItems.length-1 == i) {
+                    itemContentHeight = $(itemContent).height();
+                    windowHeight = $(window).height();
+                    itemOffset = windowHeight - itemContentHeight;
+                    page = $(itemContent).offset().top - offsetTop - itemOffset;
+                    $(itemContent).css('min-height', windowHeight/1.5)
+                }
                 if (windowTop > page) {
                     navItems.removeClass("active");
                     navItems.eq(i).addClass("active");
                 }
-                console.log(windowTop);
-                console.log(page);
             }
         }
         $(function(){
