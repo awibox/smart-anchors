79f6ae5ba8bb5b625923e74d899aa24b0167824c
necobaka <necobaka@gmail.com>
2013-10-31 01:52:04 -0700

    do refactoring

--- jquery.smartanchors.js
+++ jquery.smartanchors.js
@@ -11,48 +11,39 @@
  */
 (function($) {
     jQuery.fn.smartanchors = function(speed) {
-        var idAnchors = this.selector;
-        var offsetTop = 100;
-        var page,
-            itemContent,
-            itemContentHeight,
-            windowHeight,
-            itemOffset,
-            navItems,
-            windowTop;
-        function scrollPage() {
-            windowTop = $(window).scrollTop();
-            navItems = $(idAnchors).find("a");
-            for (var i= 0, size = navItems.length; i<size; i++) {
-                itemContent = navItems.eq(i).attr("href");
-                page = $(itemContent).offset().top - offsetTop;
-                if(navItems.length-2 == i) {
-                    $(itemContent).css('min-height', windowHeight/1.5)
-                    console.log(windowHeight)
-                }
-                if(navItems.length-1 == i) {
-                    itemContentHeight = $(itemContent).height();
-                    windowHeight = $(window).height();
-                    itemOffset = windowHeight - itemContentHeight;
-                    page = $(itemContent).offset().top - offsetTop - itemOffset;
-                    $(itemContent).css('min-height', windowHeight/1.5)
-                }
-                if (windowTop > page) {
-                    navItems.removeClass("active");
-                    navItems.eq(i).addClass("active");
-                }
-            }
-        }
-        $(function(){
-            scrollPage();
-            $(window).scroll(scrollPage);
-            $(window).resize(scrollPage);
-            navItems.click(function(){
-                event.preventDefault();
-                var itemBlock = $(this).attr("href");
-                var itemBlockScroll = $(itemBlock).offset().top;
-                $('html,body').animate ({scrollTop: itemBlockScroll},speed)
-            });
+        var me = this,
+            scrollPage = function() {
+                var navItems = $(me).find('a'),
+                    windowHeight = $(window).height(),
+                    offsetTop = 100,
+                    page,
+                    windowTop = $(window).scrollTop();
+
+                navItems.each(function(i){
+                    var content = $($(this).attr("href"));
+                    page = content.offset().top - offsetTop;
+                    if(navItems.length-2 == i) {
+                        content.css('min-height', windowHeight/1.5);
+                        console.log(windowHeight)
+                    }
+                    if(navItems.length-1 == i) {
+                        page = content.offset().top - offsetTop - windowHeight + content.height();
+                        content.css('min-height', windowHeight/1.5)
+                    }
+                    if (windowTop > page) {
+                        navItems.removeClass("active");
+                        $(this).addClass("active");
+                    }
+                });
+            };
+
+        scrollPage();
+        $(window).scroll(scrollPage);
+        $(window).resize(scrollPage);
+        $(this).find('a').click(function(ev){
+            var hash = $(this).attr("href");
+            $('html,body').animate({scrollTop: $(hash).offset().top},speed);
+            return false;
         });
     };
 })(jQuery);
